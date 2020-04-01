e91db55f80617d4e4df384efd33551d1d7681b1e
emil <amxura@gmail.com>
2020-04-01 15:30:36 +0200

    Working app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,45 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="text-center">
+      <h1>KEANet</h1>
+      <div>
+        <input type="checkbox" id="chkInternetConnection" v-model="internet">
+        <label for="chkInternetConnection">Internet connection</label>
+        <br>
+        <label for="txtPhoneLines">Phone lines</label>
+        <input type="number" id="txtPhoneLines" value="0" min="0" max="8" v-model="phoneLines">
+        <br>
+        <label>Cell phones:</label>
+        <br>
+        <table>
+          <tr>
+            <td>
+              <select id="txtCellPhones" size="5" v-model="phoneToSelect">
+                <option value="Motorola G99">Motorola G99</option>
+                <option value="iPhone 99">iPhone 99</option>
+                <option value="Samsung Galaxy 99">Samsung Galaxy 99</option>
+                <option value="Sony Xperia 99">Sony Xperia 99</option>
+                <option value="Huawei 99">Huawei 99</option>
+              </select>
+            </td>
+            <td>
+              <input type="button" value="&gt;" v-on:click="selectPhone()">
+              <br>
+              <input type="button" value="&lt;" v-on:click="deselectPhone(phoneToDeselect)">
+            </td>
+            <td>
+              <select id="txtChosenCellPhones" size="5" style="width: 150px" v-model="phoneToDeselect">
+                <option v-bind:key="phone" v-for="phone in phones">{{phone}}</option>
+              </select>
+            </td>
+          </tr>
+        </table>
+        <br>
+        Total price: {{price}} DKK
+        <br>
+        <input type="submit" value="Buy">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,6 +48,34 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      phoneToSelect: "",
+      phoneToDeselect: "",
+      phones: [],
+      internet: false,
+      phoneLines: 0
+    }
+  },
+  methods: {
+    selectPhone() {
+      console.log(this.phoneToSelect)
+      this.phones.push(this.phoneToSelect)
+    },
+    deselectPhone(){
+
+      this.phones.splice(this.phones.indexOf(this.phoneToDeselect), 1)
+
+    }
+  },
+  computed: {
+    price() {
+      var tempPrice = 0;
+      if (this.internet) tempPrice += 200;
+      tempPrice += (150 * this.phoneLines);
+      return tempPrice
+    }
   }
 }
 </script>
