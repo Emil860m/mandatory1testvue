df0955d7b7c86d6fa7c12860d6b3f6d04496420e
Nohbdys <Billen93@outlook.com>
2020-04-02 16:45:46 +0200

    Updated DataProvider

--- src/components/HelloWorld.test.js
+++ src/components/HelloWorld.test.js
@@ -26,7 +26,6 @@ test('test price', () =>
     expect(functions.price(internet, phoneLines, phones, phonePrices)).not.toBe(1799);
     expect(functions.price(internet, phoneLines, phones, phonePrices)).not.toBe("a string");
 
-
     functions.deselectPhone(phones, "Motorola G99");
     expect(functions.price(internet, phoneLines, phones, phonePrices)).toBe(1000);
 
@@ -49,50 +48,115 @@ describe('phonePrices - each', () => {
         ['Motorola G99' , 800 , true],
         ['Samsung Galaxy 99' ,  1000 , true],
         ['Motorola G99' ,  800, true]
-    ])('selectPhone (phones, "%s") - %s', (input, expectedPrice , boolResult) => {
+    ])('selectPhone (phones, "%s") - %s, %s', (input, expectedPrice , boolResult) => {
 
-        // Reset phones per test
-        phones = [];
-
-        functions.selectPhone(phones, input)
+                functions.selectPhone(phones, input)
 
         const result = functions.price(internet, phoneLines, phones, phonePrices)
 
         if ( result == expectedPrice)
         {
             boolResult = true;
+            expect(boolResult).toBe(true)
         }
         else
         {
             boolResult = false;
+            expect(boolResult).toBe(false)
         }
 
-        expect(boolResult).toBe(true)
+        // Reset values per test
+        ResetTestValues()
     }),
 
+        test.each([
+            ['Motorola G99','Samsung Galaxy 99' , 800 + 1000 , true],
+            ['Motorola G99','Huawei 99' , 800 + 900 , true],
+            ['Motorola G99','Samsung Galaxy 99' , -800 + -1000 , false],
+            ['Samsung Galaxy 99','iPhone 99' ,  8523 + 6000 , false]
+        ])('selectPhone (phones, "%s" + "%s") - %s, %s', (input1, input2, expectedPrice , boolResult) => {
+
+
+
+
+            functions.selectPhone(phones, input1)
+            functions.selectPhone(phones, input2)
+
+            const result = functions.price(internet, phoneLines, phones, phonePrices)
+
+            if ( result == expectedPrice)
+            {
+                boolResult = true;
+                expect(boolResult).toBe(true)
+            }
+            else
+            {
+                boolResult = false;
+                expect(boolResult).toBe(false)
+            }
+
+            // Reset values per test
+            ResetTestValues()
+        }),
+
+        test.each([
+            [0 , 0, true],
+            [1, 150, true],
+            [7, 1050, true],
+            [8, 1200, true],
+            [9, 1350, false],
+            [-1, -150, false]
+        ])('selectPhone (phones, "%s") - %s, %s', (input, expectedPrice, boolResult) => {
+
+
+            const result = functions.price(internet, phoneLines, phones, phonePrices)
+
+            if ( result == expectedPrice)
+            {
+                boolResult = true;
+                expect(boolResult).toBe(true)
+            }
+            else
+            {
+                boolResult = false;
+                expect(boolResult).toBe(false)
+            }
+
+            // Reset values per test
+            ResetTestValues()
+
+        }),
     test.each([
-        ['Motorola G99','Samsung Galaxy 99' , 800 + 1000 , true],
-        ['Samsung Galaxy 99','iPhone 99' ,  1000 + 6000 , true ],
-        ['Motorola G99','Huawei 99' , 800 + 900 , true]
-    ])('selectPhone (phones, "%s" + "%s") - %s', (input1, input2, expectedPrice , boolResult) => {
+        [true, 200, true],
+        [false, 0, true],
+        [true, 0, false],
+        [false, 200, false],
+        [true, -1, false]
 
-        // Reset phones per test
-        phones = [];
 
-        functions.selectPhone(phones, input1)
-        functions.selectPhone(phones, input2)
+    ])('selectPhone (phones, "%s") - %s, %s', (input, expectedPrice,  boolResult) => {
+
+        internet = input
 
         const result = functions.price(internet, phoneLines, phones, phonePrices)
-        
-        if ( result == expectedPrice)
+         if (result == expectedPrice)
         {
-            boolResult = true;
+            expect(boolResult).toBe(true)
         }
         else
         {
-            boolResult = false;
+            expect(boolResult).toBe(false)
         }
 
-        expect(boolResult).toBe(true)
+        // Reset values per test
+        ResetTestValues()
+
     })
 })
+
+function ResetTestValues(){
+    // Reset values per test
+    phones = [];
+    phoneLines = 0;
+    internet = false;
+}
